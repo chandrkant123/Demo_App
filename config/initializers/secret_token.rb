d604f2a99b4f0ef92a3fb263b887c61ab6a739d5
Raman <raman@raman-Inspiron-3521.(none)>
2014-02-19 11:19:39 +0530

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kkkkkk::Application.config.secret_token = 'cac1c39503411bc2c48388a5ccc736a16641550c2d6286751a3617fb4fcff9bc563c27439d7d4eb80a72a9a8a0272ad8aeaab2a23197f1f903911fe580e182e9'
